6e43f8750bdb92c4f74a33f6fcd8570c9a7b3c64
nastydolgova <nasty.dolgova@gmail.com>
2022-12-30 01:15:44 +0300

    add layout liderboard

---
+++ packages/client/src/components/pages/leaderboard/Leaderboard.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Leaderboard.css'
+
+function Leaderboard() {
+    return <main className="leaderboard">
+                <div className="centered-box">
+                    <div className="centered-box__inner">
+                        <h1 className="liders__title">Лидеры игры</h1>
+                        <div className="liders__wrp">
+                            <div className="liders__item">
+                                <img className="liders__img" src="#" width="48" height="48" />
+                                <span className="liders__name">
+                                    NickName
+                                </span>
+                                <span className="liders__score">
+                                    56806
+                                </span>
+                                <span className="liders__place">
+                                    1
+                                </span>
+                            </div>
+                            <div className="liders__item">
+                                <img className="liders__img" src="#" width="48" height="48" />
+                                <span className="liders__name">
+                                    NickName
+                                </span>
+                                <span className="liders__score">
+                                    56806
+                                </span>
+                                <span className="liders__place">
+                                    1
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </main>
+}
+
+export default Leaderboard
