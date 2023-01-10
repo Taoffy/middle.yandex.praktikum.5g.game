a0c8b227941621b0b7919257a62f9c4f6bba014c
Andrew Tereshin <andreyarbul@gmail.com>
2023-01-10 19:04:06 +0400

    fixed scss module

--- packages/client/src/components/pages/leaderboard/Leaderboard.tsx
+++ packages/client/src/components/pages/leaderboard/Leaderboard.tsx
@@ -1,40 +1,37 @@
-import React from 'react'
-import './Leaderboard.css'
+import React from 'react';
+
+import styles from './Leaderboard.module.scss';
 
 function LeaderboardPage() {
-    return <main className="leaderboard">
-                <div className="centered-box">
-                    <div className="centered-box__inner">
-                        <h1 className="liders__title">Лидеры игры</h1>
-                        <div className="liders__wrp">
-                            <div className="liders__item">
-                                <img className="liders__img" src="#" width="48" height="48" />
-                                <span className="liders__name">
-                                    NickName
-                                </span>
-                                <span className="liders__score">
-                                    56806
-                                </span>
-                                <span className="liders__place">
-                                    1
-                                </span>
-                            </div>
-                            <div className="liders__item">
-                                <img className="liders__img" src="#" width="48" height="48" />
-                                <span className="liders__name">
-                                    NickName
-                                </span>
-                                <span className="liders__score">
-                                    56806
-                                </span>
-                                <span className="liders__place">
-                                    1
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        </main>
+  return (
+    <main className={styles.leaderboard}>
+      <div className={styles.centeredBox}>
+        <div className={styles.centeredBox__inner}>
+          <h1 className={styles.liders__title}>Лидеры игры</h1>
+          <div className={styles.liders__wrapper}>
+            <div className={styles.liders__item}>
+              <img className={styles.liders__img} src="#" />
+              <span className={styles.liders__name}>NickName</span>
+              <span className={styles.liders__score}>56806</span>
+              <span className={styles.liders__place}>1</span>
+            </div>
+            <div className={styles.liders__item}>
+              <img className={styles.liders__img} src="#" />
+              <span className={styles.liders__name}>NickName</span>
+              <span className={styles.liders__score}>56806</span>
+              <span className={styles.liders__place}>1</span>
+            </div>
+            <div className={styles.liders__item}>
+              <img className={styles.liders__img} src="#" />
+              <span className={styles.liders__name}>NickName</span>
+              <span className={styles.liders__score}>56806</span>
+              <span className={styles.liders__place}>1</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export { LeaderboardPage };
