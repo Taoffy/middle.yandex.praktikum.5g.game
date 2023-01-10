988990bef49cc071e03555d55106a32f945baa15
Andrew Tereshin <andreyarbul@gmail.com>
2023-01-10 19:15:08 +0400

    fixed scss modules

--- packages/client/src/components/pages/login/Login.tsx
+++ packages/client/src/components/pages/login/Login.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import './Login.css';
+
+import styles from './Login.module.scss';
 
 function LoginPage() {
   return (
-    <main className="login">
-      <div className="centered-box">
-        <div className="centered-box__inner">
-          <h1 className="login__title">Авторизация</h1>
-          <form className="form__wrp">
-            <div className="input-box">
-              <label></label>
-              <input type="text" className="input" placeholder="Логин" />
+    <main className={styles.login}>
+      <div className={styles.centeredBox}>
+        <div className={styles.centeredBox__inner}>
+          <h1 className={styles.login__title}>Авторизация</h1>
+          <form className={styles.form__wrapper}>
+            <div className={styles.inputBox}>
+              <input type="text" className={styles.input} placeholder="Логин" />
             </div>
-            <div className="input-box">
-              <input type="password" className="input" placeholder="Пароль" />
+            <div className={styles.inputBox}>
+              <input
+                type="password"
+                className={styles.input}
+                placeholder="Пароль"
+              />
             </div>
-            <button className="form__btn-submit" type="submit">
+            <button className={styles.form__btnSubmit} type="submit">
               Войти
             </button>
           </form>
-          <a className="form__link" href="">
+          <a className={styles.form__link} href="#">
             Зарегистрироваться
           </a>
         </div>
