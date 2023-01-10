4997fcf5cecdeb3be86a51a2d3286c9dca21830d
Andrew Tereshin <andreyarbul@gmail.com>
2023-01-10 22:15:17 +0400

    small fixes

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { GeneralRoutes } from './components/routes/GeneralRoutes';
