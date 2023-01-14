a9f12035f5cb4eef001919ed290091c35efd5e2b
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2023-01-14 18:21:49 +0400

    Update app.d.ts

--- packages/client/src/core/types/app.d.ts
+++ packages/client/src/core/types/app.d.ts
@@ -1,23 +1,13 @@
 declare global {
   type User = {
-    id: number
-    firstName: string
-    secondName: string
-    displayName: string
-    login: string
-    email: string
-    phone: string
-    avatar: string | null
-  }
-  type UserServer = {
-    id: number
-    first_name: string
-    second_name: string
-    display_name: string
-    login: string
-    email: string
-    phone: string
-    avatar: string | null
-  }
+    id: number;
+    first_name: string;
+    second_name: string;
+    display_name: string;
+    login: string;
+    email: string;
+    phone: string;
+    avatar: string | null;
+  };
 }
-export {}
+export {};
