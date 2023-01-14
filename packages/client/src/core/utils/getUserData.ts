c2b0fd42e8880531d1f75e994335d62720c524e5
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2023-01-14 15:44:19 +0400

    userService

---
+++ packages/client/src/core/utils/getUserData.ts
@@ -0,0 +1,14 @@
+import { serverURL } from '../config/api.config'
+
+export const getUserData = (data: UserServer): User => {
+  return {
+    id: data.id,
+    login: data.login,
+    firstName: data.first_name,
+    secondName: data.second_name,
+    displayName: data.display_name,
+    avatar: data.avatar ? `${serverURL}/resources/${data.avatar}` : null,
+    phone: data.phone,
+    email: data.email,
+  }
+}
