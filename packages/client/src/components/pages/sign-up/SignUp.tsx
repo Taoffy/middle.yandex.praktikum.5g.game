50ec4ab05a60fc2b949b3710ca8920963edbb016
Andrew Tereshin <andreyarbul@gmail.com>
2023-01-26 20:43:59 +0400

    fixed styles issues

--- packages/client/src/components/pages/sign-up/SignUp.tsx
+++ packages/client/src/components/pages/sign-up/SignUp.tsx
@@ -1,24 +1,50 @@
-import React from 'react'
-import styles from './SignUp.module.scss'
+import React from 'react';
+import styles from './SignUp.module.scss';
 
 function SignUpPage() {
   return (
-    <div className={styles.registration__container}>
-      <h1 className={styles.registration__title}>Регистрация</h1>
+    <div className={styles.registration}>
+      <div className={styles.registration__container}>
+        <h1 className={styles.registration__title}>Регистрация</h1>
 
-      <form className={styles.registration__form} action=''>
-        <input className={styles.registration__item} type='text' placeholder='Имя'/>
-        <input className={styles.registration__item} type='text' placeholder='Фамилия'/>
-        <input className={styles.registration__item} type='text' placeholder='Логин'/>
-        <input className={styles.registration__item} type='text' placeholder='E-mail'/>
-        <input className={styles.registration__item} type='text' placeholder='Телефон'/>
+        <form className={styles.registration__form} action="">
+          <input
+            className={styles.registration__item}
+            type="text"
+            placeholder="Имя"
+          />
+          <input
+            className={styles.registration__item}
+            type="text"
+            placeholder="Фамилия"
+          />
+          <input
+            className={styles.registration__item}
+            type="text"
+            placeholder="Логин"
+          />
+          <input
+            className={styles.registration__item}
+            type="text"
+            placeholder="E-mail"
+          />
+          <input
+            className={styles.registration__item}
+            type="text"
+            placeholder="Телефон"
+          />
 
-        <button className={styles.registration__btn} type='submit'>Зарегистрироваться</button>
-      </form>
+          <button className={styles.registration__btn} type="submit">
+            Зарегистрироваться
+          </button>
+        </form>
 
-      <a className={styles.registration__login} href='/login'>Войти</a>
+        <a className={styles.registration__login} href="/login">
+          Войти
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export { SignUpPage };
