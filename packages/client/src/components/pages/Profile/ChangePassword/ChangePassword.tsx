8818f73d3f27c261a60d4454446f1483b90726f1
dakryuchkov <dakryuchkov@phoenixit.ru>
2023-02-07 19:37:35 +0300

    5G-30 refactoring

--- packages/client/src/components/pages/Profile/ChangePassword/ChangePassword.tsx
+++ packages/client/src/components/pages/Profile/ChangePassword/ChangePassword.tsx
@@ -1,14 +1,9 @@
-import React, { useRef } from 'react'
-import { connect, useDispatch } from 'react-redux'
-import styles from './ChangePassword.module.scss'
-import * as Actions from '../../../../redux/actions'
+import React, { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import styles from './ChangePassword.module.scss';
+import * as Actions from '../../../../redux/actions';
 
-type ChangePasswordProps = {
-    oldPassword: string;
-    newPassword: string;
-}
-
-function ChangePassword(props: ChangePasswordProps) {
+function ChangePassword() {
   const dispatch = useDispatch();
   const formEl = useRef(null);
 
@@ -20,33 +15,38 @@ function ChangePassword(props: ChangePasswordProps) {
     const data = {
       oldPassword: formData.get('oldPassword'),
       newPassword: formData.get('newPassword'),
-    }
+    };
 
     dispatch(Actions.changeUserPassword(data));
-  }
+  };
 
   return (
     <div className={styles.password__container}>
       <h1 className={styles.password__header}> Изменить пароль</h1>
-      <form id='change-password' action='' onSubmit={onPasswordSubmit} ref={formEl}>
-        <input className={styles.password__input} type='password' placeholder='Старый пароль' name='oldPassword' />
-        <input className={styles.password__input} type='password' placeholder='Новый пароль'  name='newPassword'  />
+      <form
+        id="change-password"
+        action=""
+        onSubmit={onPasswordSubmit}
+        ref={formEl}>
+        <input
+          className={styles.password__input}
+          type="password"
+          placeholder="Старый пароль"
+          name="oldPassword"
+        />
+        <input
+          className={styles.password__input}
+          type="password"
+          placeholder="Новый пароль"
+          name="newPassword"
+        />
 
-        <button className={styles.password__btn} type='submit'>Изменить пароль</button>
+        <button className={styles.password__btn} type="submit">
+          Изменить пароль
+        </button>
       </form>
     </div>
   );
 }
 
-const mapStateToProps = (state: any) => {
-  return {
-    first_name: state.app.user.first_name,
-    second_name: state.app.user.second_name,
-    display_name: state.app.user.display_name,
-    login: state.app.user.login,
-    email: state.app.user.email,
-    phone: state.app.user.phone,
-  };
-};
-
-export default connect(mapStateToProps)(ChangePassword);
+export default ChangePassword;
--- packages/client/src/components/pages/Profile/Profile.tsx
+++ packages/client/src/components/pages/Profile/Profile.tsx
@@ -7,11 +7,7 @@ import { Store } from '../../../redux/types';
 
 function Profile() {
   const [isPopupActive, setPopupActive] = useState(false);
-  const first_name = useSelector((state: Store) => state.app.user.first_name);
-  const second_name = useSelector((state: Store) => state.app.user.second_name);
-  const login = useSelector((state: Store) => state.app.user.login);
-  const email = useSelector((state: Store) => state.app.user.email);
-  const phone = useSelector((state: Store) => state.app.user.phone);
+  const user = useSelector((state: Store) => state.app.user);
 
   return (
     <div className={styles.profile}>
@@ -28,23 +24,23 @@ function Profile() {
         <ul className={styles.profile__list}>
           <li className={styles.profile__item}>
             <span className={styles.profile__subtitle}>Имя:</span>
-            <span className={styles.profile__info}>{first_name}</span>
+            <span className={styles.profile__info}>{user.first_name}</span>
           </li>
           <li className={styles.profile__item}>
             <span className={styles.profile__subtitle}>Фамилия:</span>
-            <span className={styles.profile__info}>{second_name}</span>
+            <span className={styles.profile__info}>{user.second_name}</span>
           </li>
           <li className={styles.profile__item}>
             <span className={styles.profile__subtitle}>Логин:</span>
-            <span className={styles.profile__info}>{login}</span>
+            <span className={styles.profile__info}>{user.login}</span>
           </li>
           <li className={styles.profile__item}>
             <span className={styles.profile__subtitle}>E-mail:</span>
-            <span className={styles.profile__info}>{email}</span>
+            <span className={styles.profile__info}>{user.email}</span>
           </li>
           <li className={styles.profile__item}>
             <span className={styles.profile__subtitle}>Телефон:</span>
-            <span className={styles.profile__info}>{phone}</span>
+            <span className={styles.profile__info}>{user.phone}</span>
           </li>
         </ul>
 
