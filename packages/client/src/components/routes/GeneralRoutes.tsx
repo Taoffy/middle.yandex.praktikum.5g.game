9c7147b9cfff308d6308808a35c93fc5f2674b8f
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2023-02-12 18:25:08 +0300

    Update GeneralRoutes.tsx

--- packages/client/src/components/routes/GeneralRoutes.tsx
+++ packages/client/src/components/routes/GeneralRoutes.tsx
@@ -17,12 +17,12 @@ const GeneralRoutes = () => {
       <Route path="/">
         <Route path="login" element={<LoginPage />} />
         <Route path="sign-up" element={<SignUpPage />} />
-        <Route path="main" element={<MainPage />} />
-        <Route path="about-game" element={<AboutGame />} />
-        <Route path="forum" element={<ForumPage />} />
-        <Route path="forumpost" element={<ForumPostPage />} />
-        <Route path="leaderboard" element={<LeaderboardPage />} />
-        <Route path="profile" element={<Profile />} />
+        <Route path="main" element={withAuth(MainPage)({})} />
+        <Route path="about-game" element={withAuth(AboutGame)({})} />
+        <Route path="forum" element={withAuth(ForumPage)({})} />
+        <Route path="forumpost" element={withAuth(ForumPostPage)({})} />
+        <Route path="leaderboard" element={withAuth(LeaderboardPage)({})} />
+        <Route path="profile" element={withAuth(Profile)({})} />
       </Route>
     </Routes>
   );
