13db833c412fdead4746aa694b1c1872eb6c22c7
dakryuchkov <dakryuchkov@phoenixit.ru>
2023-02-14 22:22:12 +0300

    5G-19 SSR without routing

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement,
-  <React.StrictMode>
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </BrowserRouter>
 );
--- packages/client/ssr.tsx
+++ packages/client/ssr.tsx
@@ -1,15 +1,15 @@
-import App from './src/App'
-import { renderToString } from 'react-dom/server'
-import { StaticRouter } from "react-router-dom/server";
-import { Provider } from 'react-redux'
-import store from './src/redux/store'
+import App from './src/App';
+import { renderToString } from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 
-export function render() {
+export function render(url, context) {
   return renderToString(
-    <StaticRouter>
+    <StaticRouter location={url} context={context}>
       <Provider store={store}>
         <App />
       </Provider>
     </StaticRouter>
-  )
+  );
 }
