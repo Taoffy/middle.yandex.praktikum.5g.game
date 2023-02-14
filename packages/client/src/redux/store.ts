719dee791b332e67a698bf4e5807c91522376b93
dakryuchkov <dakryuchkov@phoenixit.ru>
2023-02-14 22:34:09 +0300

    5G-19 SSR with login route

--- packages/client/src/redux/store.ts
+++ packages/client/src/redux/store.ts
@@ -10,10 +10,15 @@ declare global {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
   }
 }
-
+const composeEnchancers = compose(
+  typeof window !== 'undefined' && window.devToolsExtension
+    ? window.devToolsExtension()
+    : (f) => f
+);
 
 const store = createStore(
-  reducers
+  reducers,
+  composeEnchancers(applyMiddleware(thunk as ThunkMiddleware))
 );
 
 export default store;
