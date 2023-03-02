bd3bd7959868ce972197b5fe78533ac9b6d2311e
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2023-03-02 10:59:22 +0300

    Update withOauth.tsx

--- packages/client/src/components/hoc/withOauth.tsx
+++ packages/client/src/components/hoc/withOauth.tsx
@@ -7,26 +7,26 @@ import {
 } from '../hook/AppUseSelectorAndDispathch';
 
 const withOauth = (Component: ComponentType): FC => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const isAuth = useAppSelector(({ app }) => app.isAuth);
+  const isInit = useAppSelector(({ app }) => app.init);
+  useEffect(() => {
+    if (isAuth) {
+      navigate('/main');
+      return;
+    }
+  }, [isAuth]);
+  useEffect(() => {
+    const code = searchParams.get('code');
+    if (code && !isAuth) {
+      dispatch(Actions.signinOAuth(code));
+    } else if (isInit) {
+      navigate('/login');
+    }
+  }, [isInit]);
   const hoc = (props: any) => {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const isAuth = useAppSelector(({ app }) => app.isAuth);
-    const isInit = useAppSelector(({ app }) => app.init);
-    useEffect(() => {
-      if (isAuth) {
-        navigate('/main');
-        return;
-      }
-    }, [isAuth]);
-    useEffect(() => {
-      const code = searchParams.get('code');
-      if (code && !isAuth) {
-        dispatch(Actions.signinOAuth(code));
-      } else if (isInit) {
-        navigate('/login');
-      }
-    }, [isInit]);
     return <Component {...props} />;
   };
   return hoc;
