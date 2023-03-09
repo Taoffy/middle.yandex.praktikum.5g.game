b66ce90b5cdcc033edf353d511bd2198fb8b363c
MrToddest <sadofjevdenis@yandex.ru>
2023-03-09 10:32:35 +0500

    add leaderboard

---
+++ packages/client/src/api/leaderboardAPI.ts
@@ -0,0 +1,35 @@
+import { api } from '../core/api';
+
+export type LeaderboardObject = {
+    name: string;
+    score: number;
+    avatar: string;
+}
+export type LeaderboardRequestData = {
+    data: LeaderboardObject;
+    ratingFieldName: string;
+    teamName: string;
+}
+export type LeaderboardListData = {
+    ratingFieldName: string;
+    cursor: number;
+    limit: number;
+}
+
+class Leaderboard {
+    async addToLeaderboard(data: LeaderboardRequestData) {
+        const response = await api.put('/leaderboard', data, {
+            withCredentials: true,
+            headers: { 'Content-Type': 'application/json' },
+        }).then((res) => res.data);
+        return response;
+    }
+    async getLeaderboard(data: LeaderboardListData) {
+        const response = await api.put('/leaderboard/5g', data, {
+            withCredentials: true,
+            headers: { 'Content-Type': 'application/json' },
+        }).then((res) => res.data);
+        return response;
+    }
+}
+export default new Leaderboard();
--- packages/client/src/components/pages/leaderboard/Leaderboard.tsx
+++ packages/client/src/components/pages/leaderboard/Leaderboard.tsx
@@ -1,32 +1,40 @@
 import React from 'react';
+import Leaderboard from '../../../api/leaderboardAPI';
 
 import styles from './Leaderboard.module.scss';
 
 function LeaderboardPage() {
+  const leaders = Leaderboard.getLeaderboard({
+    ratingFieldName: "score",
+    cursor: 0,
+    limit: 5
+  })
+
+  // const leaders = [{
+  //   name: "Новый",
+  //   score: 15000,
+  //   place: 1,
+  //   avatar: 'path/to/avatar.jpg'
+  // }] пример вывода
+
   return (
     <main className={styles.leaderboard}>
       <div className={styles.centeredBox}>
         <div className={styles.centeredBox__inner}>
           <h1 className={styles.liders__title}>Лидеры игры</h1>
           <div className={styles.liders__wrapper}>
-            <div className={styles.liders__item}>
-              <img className={styles.liders__img} src="#" />
-              <span className={styles.liders__name}>NickName</span>
-              <span className={styles.liders__score}>56806</span>
-              <span className={styles.liders__place}>1</span>
-            </div>
-            <div className={styles.liders__item}>
-              <img className={styles.liders__img} src="#" />
-              <span className={styles.liders__name}>NickName</span>
-              <span className={styles.liders__score}>56806</span>
-              <span className={styles.liders__place}>1</span>
-            </div>
-            <div className={styles.liders__item}>
-              <img className={styles.liders__img} src="#" />
-              <span className={styles.liders__name}>NickName</span>
-              <span className={styles.liders__score}>56806</span>
-              <span className={styles.liders__place}>1</span>
-            </div>
+            {
+              leaders.map((value, _)=>{
+                return (
+                  <div className={styles.liders__item}>
+                    <img className={styles.liders__img} src={"#"+value.avatar} />
+                    <span className={styles.liders__name}>{value.name}</span>
+                    <span className={styles.liders__score}>{value.score}</span>
+                    <span className={styles.liders__place}>{value.place}</span>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
       </div>
