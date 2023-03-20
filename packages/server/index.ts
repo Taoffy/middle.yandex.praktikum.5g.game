5888e0cfd547ccbbf2f9c12fe677f89116da1099
dakryuchkov <dakryuchkov@phoenixit.ru>
2023-03-20 18:38:25 +0300

    5G-59 refactoring CSP

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -13,6 +13,12 @@ import { INITIAL_STATE } from './utils';
 
 const app = express();
 app.use(cors());
+app.use(function(_req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy', "connect-src *; default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self' 'unsafe-inline' https://ya-praktikum.tech/api/v2/*; style-src 'self'; frame-src 'self'; base-uri 'self'; worker-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none'"
+  );
+  next();
+});
 
 const isDev = () => process.env.NODE_ENV === 'development';
 
@@ -35,7 +41,7 @@ async function startServer() {
     vite = await createViteServer({
       server: { middlewareMode: true },
       root: srcPath,
-      appType: 'custom',
+      appType: 'custom'
     });
 
     app.use(vite.middlewares);
