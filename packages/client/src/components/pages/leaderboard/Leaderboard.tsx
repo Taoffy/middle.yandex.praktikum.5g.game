9794a3102da0d9095da7670a8589f1943c521683
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2023-03-20 23:26:34 +0300

    fix

--- packages/client/src/components/pages/leaderboard/Leaderboard.tsx
+++ packages/client/src/components/pages/leaderboard/Leaderboard.tsx
@@ -1,35 +1,41 @@
 import React, { useState } from 'react';
 import leaderboardAPI from '../../../api/leaderboardAPI';
-import svg from '../../../../public/vite.svg'
+import svg from '../../../../public/vite.svg';
 import styles from './Leaderboard.module.scss';
 import { LeaderboardResultData } from '../../../api/leaderboardAPI';
 
 function LeaderboardPage() {
-  const [leaders, setLeaders] = useState<LeaderboardResultData>()
+  const [leaders, setLeaders] = useState<LeaderboardResultData>();
   useState(() => {
     leaderboardAPI.getLeaderboard().then((x) => {
-      setLeaders(x)
-    })
-  })
+      setLeaders(x);
+    });
+  });
   return (
     <main className={styles.leaderboard}>
       <div className={styles.centeredBox}>
         <div className={styles.centeredBox__inner}>
           <h1 className={styles.liders__title}>Лидеры игры</h1>
-          {
-            // leaders.map((value, key) => {
-            //   return (
-            //     <div className={styles.liders__wrapper}>
-            //       <div className={styles.liders__item}>
-            //         <img className={styles.liders__img} src={svg} />
-            //         <span className={styles.liders__name}>{value.data.name}</span>
-            //         <span className={styles.liders__score}>{value.data.score}</span>
-            //         <span className={styles.liders__place}>{key + 1}</span>
-            //       </div>
-            //     </div>
-            //   )
-            // })
-          }
+          {leaders ? (
+            leaders.map((value, key) => {
+              return (
+                <div key={key} className={styles.liders__wrapper}>
+                  <div className={styles.liders__item}>
+                    <img className={styles.liders__img} src={svg} />
+                    <span className={styles.liders__name}>
+                      {value.data.name}
+                    </span>
+                    <span className={styles.liders__score}>
+                      {value.data.score}
+                    </span>
+                    <span className={styles.liders__place}>{key + 1}</span>
+                  </div>
+                </div>
+              );
+            })
+          ) : (
+            <div>список пуст</div>
+          )}
         </div>
       </div>
     </main>
