ccbe24430f47449427a7c8c40afe5deb8b9ff9b4
antonabaltusov <76466614+antonabaltusov@users.noreply.github.com>
2023-03-22 23:58:43 +0300

    Update api.config.ts

--- packages/client/src/core/config/api.config.ts
+++ packages/client/src/core/config/api.config.ts
@@ -1,9 +1,7 @@
 export const serverURL = 'https://ya-praktikum.tech/api/v2';
-export const oauthCallback =
-  import.meta.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000'
-    : import.meta.env.OAUTH_CALLBACK;
-export const expressApi =
-  import.meta.env.NODE_ENV === 'development'
-    ? 'http://localhost:3001/api'
-    : import.meta.env.EXPRESS_API;
+export const oauthCallback = import.meta.env.EXPRESS_API
+  ? import.meta.env.OAUTH_CALLBACK
+  : 'http://localhost:3000';
+export const expressApi = import.meta.env.EXPRESS_API
+  ? import.meta.env.EXPRESS_API
+  : 'http://localhost:3001/api';
