4bd9c83f60ed60176e658a7e2330d8797165bac4
Andrew Tereshin <andreyarbul@gmail.com>
2023-03-23 11:44:44 +0400

    fixed env

--- init.js
+++ init.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
 
-fs.copyFileSync('.env', '.env');
+fs.copyFileSync('.env.sample', '.env');
 
 fs.mkdirSync('tmp/pgdata', { recursive: true });
