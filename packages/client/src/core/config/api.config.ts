362d193df7d8d6bfbc35db4fce0cdf84b99212c6
Andrew Tereshin <andreyarbul@gmail.com>
2023-03-24 10:02:20 +0400

    small fixes

--- packages/client/src/core/config/api.config.ts
+++ packages/client/src/core/config/api.config.ts
@@ -1,7 +1,7 @@
 export const serverURL = 'https://ya-praktikum.tech/api/v2';
-export const oauthCallback = import.meta.env.VITE_EXPRESS_API
+export const oauthCallback = import.meta.env.VITE_OAUTH_CALLBACK
   ? import.meta.env.VITE_OAUTH_CALLBACK
-  : 'http://localhost:3000';
+  : 'http://localhost:3001';
 export const expressApi = import.meta.env.VITE_EXPRESS_API
   ? import.meta.env.VITE_EXPRESS_API
   : 'http://localhost:3001/api';
